f464998c9ebc2bbfce4513297f87b0066ffbdca8
Zhou Chang <achang.zhou@gmail.com>
2025-05-01 15:40:41 +0800

    added siflow proxy

---
+++ apiproxy/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+var cors = require('cors');
+
+const app = express();
+
+// Proxy configuration
+app.use('/v1', createProxyMiddleware({
+  target: 'https://api.siliconflow.cn', // Change this to your target server
+  onError: (err, req, res) => {
+      res.status(500).send('Proxy error');
+  }
+  //pathRewrite: {'^/api': ''},
+  changeOrigin: true,
+}));
+
+app.use(cors());
+
+const PORT = 8800;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
